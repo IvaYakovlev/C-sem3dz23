6171c4cfd7a2a050fc51e80cbef0493e6188138e
Ivan Yakovlev <mail.siiix@yandex.ru>
2023-02-05 19:02:18 +0300

    Код работает

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу которая принимает
+// на вход число (N) и выдает таблицу кубов чисел от 1 до N
+
+Console.WriteLine ("Результаты возведения в 3-ю степень скольки чисел хотите видеть?");
+int x=int.Parse (Console.ReadLine());
+int i=1;
+if (x<0){
+  i=x;
+  x=-1; // х=-1*х
+}
+for (;i<=x; i++){
+  Console.Write ($"{Math.Pow(i,3)} ");
+}
